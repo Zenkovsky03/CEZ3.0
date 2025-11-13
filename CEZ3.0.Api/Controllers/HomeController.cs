1b7e17d0e894d5575c3ab9260b289fbd095f092c
Konrad <ninianob@gmail.com>
2025-11-13 21:08:48 +0100

    Scalar documentation added

---
+++ CEZ3.0.Api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CEZ3._0.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HomeController : ControllerBase
+{
+    [HttpGet("hello")]
+    public IActionResult HelloWorld()
+    {
+        return Ok("Hello World");
+    }
+}
---
+++ CEZ3.0.Infrastructure/Extentions/ServiceCollectionExtentions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CEZ3._0.Infrastructure.Extentions;
+
+public static class ServiceCollectionExtentions
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("");
+        //services.AddDbContext<>(options => options.UseSqlServer(connectionString));
+    }
+}
